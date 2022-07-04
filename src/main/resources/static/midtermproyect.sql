83a6f4bb9c04e452654cb6a39d3ccff562148e4b
Ruth Telleria <telleriaviana@gmail.com>
2022-07-04 09:02:44 +0200

    sql actualizado

--- src/main/resources/static/midtermproyect.sql
+++ src/main/resources/static/midtermproyect.sql
@@ -21,14 +21,23 @@ password VARCHAR(255),
 PRIMARY KEY(id)
 );
 
+INSERT INTO user(username, password) VALUES
+('Lorena Pardo', '$2a$10$pA9dFNVJuQpsKc.mmTO2GeNzPm.d17IvAWGsBlUxHaRO6yFhagpZi'),
+('Ruth Telleria', '$2a$10$RF3qixgdrEWAc.SPED/.n.azg2nkjv5bIiPht340LJpUB1aMciUsS'),
+('Julen Telleria', '$2a$10$uCrKWTQuzGDkzlnf2Qe94uQ6vj/Zpt.sGTRG5egynj8oZQR6mr4de')
+;
+
 CREATE TABLE admin(
-id BIGINT NOT NULL AUTO_INCREMENT,
+id BIGINT NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (id) REFERENCES user(id)
 );
+INSERT INTO admin(id) VALUES
+(1)
+;
 
 CREATE TABLE account_holder(
-id BIGINT NOT NULL AUTO_INCREMENT,
+id BIGINT NOT NULL,
 date_birth DATE,
 street VARCHAR(255),
 post_code INT,
@@ -38,12 +47,19 @@ PRIMARY KEY (id),
 FOREIGN KEY (id) REFERENCES user(id)
 );
 
+INSERT INTO account_holder(id, date_birth, street, post_code) VALUES
+(2, '1994-07-08', 'ambasaguas 55', 48891),
+(3, '1991-06-30', 'ambasaguas 55', 48891)
+;
+
 CREATE TABLE third_party(
 id BIGINT NOT NULL AUTO_INCREMENT,
 name VARCHAR(255),
 hashed_key VARCHAR(255),
 PRIMARY KEY (id)
 );
+INSERT INTO third_party(id, name, hashed_key) VALUES
+(12, 'Aitor Gonzalez', '9876432');
 
 CREATE TABLE role(
 id BIGINT NOT NULL AUTO_INCREMENT,
@@ -52,15 +68,21 @@ user BIGINT,
 PRIMARY KEY(id),
 FOREIGN KEY (user) REFERENCES user(id)
 );
-
+INSERT INTO role(name, user) VALUES
+('ADMIN', 1),
+('USER', 1),
+('USER', 2),
+('USER', 3);
 
 CREATE TABLE account(
 id BIGINT NOT NULL AUTO_INCREMENT,
-balance DECIMAL,
+amount DECIMAL,
+currency VARCHAR (255),
 secret_key VARCHAR(255),
 primary_owner_id BIGINT,
 secondary_owner_id BIGINT,
-penalty_fee DECIMAL,
+penalty_fee_amount DECIMAL,
+penalty_fee_currency VARCHAR (255),
 creation_date DATE,
 status VARCHAR(255),
 PRIMARY KEY(id),
@@ -68,37 +90,42 @@ FOREIGN KEY(primary_owner_id) REFERENCES user(id),
 FOREIGN KEY(secondary_owner_id) REFERENCES user(id)
 );
 
+
 CREATE TABLE student_checking(
-id BIGINT NOT NULL AUTO_INCREMENT,
+id BIGINT NOT NULL,
 PRIMARY KEY(id),
 FOREIGN KEY(id) REFERENCES account(id)
 );
 
+
 CREATE TABLE checking(
-id BIGINT NOT NULL AUTO_INCREMENT,
-monthly_maintenance_fee DECIMAL,
-minimum_balance DECIMAL,
+id BIGINT NOT NULL,
+monthly_maintenance_fee_amount DECIMAL,
+monthly_maintenance_fee_currency VARCHAR (255),
+minimum_balance_amount DECIMAL,
+minimum_balance_currency VARCHAR (255),
 last_actualized_date DATE,
 PRIMARY KEY(id),
 FOREIGN KEY(id) REFERENCES account(id)
 );
 
 CREATE TABLE saving(
-id BIGINT NOT NULL AUTO_INCREMENT,
+id BIGINT NOT NULL,
 interest_rate DECIMAL,
-minimum_balance DECIMAL,
+minimum_balance_amount DECIMAL,
+minimum_balance_currency VARCHAR (255),
 last_actualized_date DATE,
 PRIMARY KEY(id),
 FOREIGN KEY(id) REFERENCES account(id)
 );
+
 
 CREATE TABLE credit_card(
-id BIGINT NOT NULL AUTO_INCREMENT,
+id BIGINT NOT NULL,
 interest_rate DECIMAL,
-credit_limit DECIMAL,
+credit_limit_amount DECIMAL,
+credit_limit_currency VARCHAR (255),
 last_actualized_date DATE,
 PRIMARY KEY(id),
 FOREIGN KEY(id) REFERENCES account(id)
 );
-
-
